9c5514193c2e43fc775d0c7432f1f5c738f9b209
Gleb Kochubey <gleb.kochubey@gmail.com>
2022-03-06 22:49:31 +0100

    Add TITLE tag for single Note

--- src/pages/note.js
+++ src/pages/note.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery, gql } from '@apollo/client';
 
 import Note from '../components/Note';
 import { GET_NOTE } from '../gql/query';
 
 const NotePage = props => {
+  useEffect(() => {
+    // Обновляем заголовок документа
+    document.title = `Note ID ${noteId} by ${String(authorUsername).replace(/^\w/, (c) => c.toUpperCase()) } — Notedly`;
+  });
+
   // Сохраняем id из url в виде переменной
   const id = props.match.params.id;
 
@@ -16,6 +21,13 @@ const NotePage = props => {
 
   // Если при получении данных произошел сбой, отображаем сообщение об ошибке
   if (error) return <p>Error! Note not found</p>;
+
+  let authorUsername;
+  let noteId;
+  if (data) {
+    authorUsername = data.note.author.username;
+    noteId = data.note.id;
+  }
 
   // Если загрузка данных произошла успешно, отображаем их в UI
   return <Note note={data.note} />;
